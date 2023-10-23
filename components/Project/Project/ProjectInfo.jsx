4cf26d0c4efc0ad4efce230de9ca58b647c7807c
Korotenko Alex <alexey.korotenko90@gmail.com>
2023-10-23 14:06:15 +0300

    add styles for projects

--- components/Project/Project/ProjectInfo.jsx
+++ components/Project/Project/ProjectInfo.jsx
@@ -1,12 +1,34 @@
 import Image from "next/image";
 import React from "react";
+import classNames from "classnames";
 
-const ProjectInfo = ({ name, desc, imageUrl, imageAlt }) => {
+import Description from "@/components/Description";
+
+const ProjectInfo = ({ name, desc, imageUrl, imageAlt, index }) => {
+  const isInteger = classNames(
+    { "md:flex-row": index % 2 === 0 },
+    { "md:flex-row-reverse": index % 2 !== 0 }
+  );
+
   return (
-    <li>
-      <p>{name}</p>
-      <p>{desc}</p>
-      <Image src={imageUrl} alt={imageAlt} width={200} height={300} />
+    <li className={`flex flex-col gap-5 items-center ${isInteger}  `}>
+      <div>
+        <Description
+          text={name}
+          className="mb-4 text-center text-sm uppercase font-medium tracking-wider"
+        />
+        <Description text={desc} className="mb-4  text-sm" />
+      </div>
+      <div>
+        <Image
+          src={imageUrl}
+          alt={imageAlt}
+          width={800}
+          height={1000}
+          loading="lazy"
+          layout="responsive"
+        />
+      </div>
     </li>
   );
 };
--- components/Project/Projects.jsx
+++ components/Project/Projects.jsx
@@ -6,7 +6,7 @@ const Projects = ({ data }) => {
   return (
     <section>
       <div className="container">
-        <ul>
+        <ul className="flex flex-col gap-7">
           {data?.map(({ _id, project, data, slug }) => (
             <Project key={_id} title={project} data={data} slug={slug} />
           ))}
